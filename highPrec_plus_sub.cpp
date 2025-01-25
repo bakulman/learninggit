4e16152c6de5e16131b1c0a0f355c9bd61d794a9
bakul <zzy204112@foxmail.com>
2025-01-25 17:08:20 +0800

    高精度加法和减法注意写法

---
+++ highPrec_plus_sub.cpp
@@ -0,0 +1,186 @@
+#include <bits/stdc++.h>
+#define de(x) cout << #x << " = " << x << endl;
+#define de2(x,y) cout << #x << " = " << x << " " << #y << " = " << y << endl;
+#define de3(x,y,z) cout << #x << " = " << x << " " << #y << " = " << y << " " << #z << " = " << z << endl;
+#define f(i,a,b) for(int i = a;i <= b;++i)
+#define fd(i,a,b) for(int i = a;i >= b;--i)
+#define fro for
+
+typedef long long ll;
+using namespace std;
+
+string L_Subtraction(string s1,string s2)
+{
+    bool neg = false;
+    int len1 = s1.size(),len2 = s2.size();
+    if(len1 < len2 || s1 < s2)//符号判定
+    {
+        neg = true;
+        swap(s1,s2);
+        swap(len1,len2);
+    }
+    vector<int> a1(len1,0),a2(len1,0),ans(len1,0);//特别注意,算减法的时候会访问到len1的长度,小心a2越界!!!
+    for(int i = 0;i < len1;++i)
+    {
+        a1[i] = s1[len1 - 1 - i]  - '0';
+    }
+
+    for(int i = 0;i < len2;++i)
+    {
+        a2[i] = s2[len2 - 1 - i]  - '0';
+    }
+
+    for(int i = 0;i < len1;++i)
+    {
+        int temp = a1[i] - a2[i];
+        if(temp< 0)
+        {
+            temp += 10;
+            a1[i+1]--;
+        }
+        ans[i] = temp;//别忘了
+    }
+
+    string anss = "";
+    if(neg)
+        anss = "-" + anss;
+    int i = len1-1;//len1-1是最末尾,小心越界
+    for(;i > 0 && ans[i]==0;++i);
+
+    for(;i >= 0;--i)
+    {
+        anss += ans[i] + '0';
+    }
+    return anss;
+}
+
+// string l_plus(string a, string b)
+// {
+//     vector<int> n(10000,0);
+//     vector<int> m(10000,0);
+//     vector<int> sum(10000,0);
+//
+//     int la = a.size();
+//     int lb = b.size();
+//
+//     for(int i = la - 1;i >= 0;--i)
+//     {
+//         n[la-i-1] = a[i] - '0';
+//         //de(i);
+//     }
+//     for(int i = lb - 1;i >= 0;--i)
+//     {
+//         m[lb-i-1] = b[i] - '0';
+//         //de(i);
+//     }
+//
+//     //add
+//     ll add = 0;
+//     int len = max(la, lb);
+//     for(int i = 0;i < len;++i)
+//     {
+//         sum[i] = n[i] + m[i] + add;
+//         add = sum[i]/10;
+//         sum[i] %= 10;
+//         //cout << sum[i] << " ";
+//
+//     }
+//     if(add!= 0)
+//     {
+//         sum[len] = add;
+//         len++;
+//         //de(len);
+//     }
+//
+//     string res = "";
+//     for(int i = len -1;i >= 0;--i)
+//     {
+//         char ch = sum[i] + '0';
+//         res += ch;
+//         //de(ch);
+//     }
+//     //cout << res;
+//
+//     return res;
+// }
+
+//
+//  string mul_2(string s)
+// {
+//     vector<int> a(1000,0);
+//     for(int i = 0;i <= s.size()-1;++i)
+//     {
+//         a[i] = s[s.size()-1-i] - '0';
+//     }
+//
+//     ll jin = 0;
+//     for(int i = 0;i < s.size();++i)
+//     {
+//         a[i]  = a[i]*2 + jin;
+//         jin = a[i]/10;
+//         a[i] %= 10;
+//     }
+//     ll len = s.size();
+//     if(jin != 0)
+//     {
+//         a[s.size()] = jin;
+//         len++;
+//     }
+//
+//
+//
+//      string res = "";
+//      for(int i = len-1;i >= 0;--i)
+//      {
+//          char ch = a[i] + '0';
+//          res += ch;
+//      }
+//      return res;
+//
+// }
+//
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    string a,b;
+    cin >> a >> b;
+    cout << L_Subtraction(a,b);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
